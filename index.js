348831ca58293be465f5ec0bb7b581194e1e6d94
lrsleandro <lrsleandro@gmail.com>
2020-02-26 14:48:34 -0300

    Exercício: implementando categorias - novo editar delete

--- index.js
+++ index.js
@@ -74,6 +74,46 @@ app.post('/admin/vagas/editar/:id', async(req, res) => {
     res.redirect('/admin/vagas')
 })
 
+app.get('/admin/categorias', async(req, res) => {
+    const db = await dbConnection
+    const categorias = await db.all('select * from categorias;')
+    res.render('admin/categorias', { categorias })
+})
+
+app.get('/admin/categorias/delete/:id', async(req, res) => {
+    const db = await dbConnection
+    await db.run('delete from categorias where id = ' + req.params.id)
+    res.redirect('/admin/categorias')
+})
+
+app.get('/admin/categorias/nova', async(req, res) => {
+    const db = await dbConnection
+    const categorias = await db.all('select * from categorias')
+    res.render('admin/nova-categoria', { categorias })
+})
+
+app.post('/admin/categorias/nova', async(req, res) => {
+    const { categoria } = req.body
+    const db = await dbConnection
+    await db.run(`insert into categorias(categoria) values('${categoria}')`)
+    res.redirect('/admin/categorias')
+})
+
+//app.get('/admin/vagas/editar/:id', async(req, res) => {
+//    const db = await dbConnection
+//    const categorias = await db.all('select * from categorias')
+//    const vaga = await db.get('select * from vagas where id = ' + req.params.id)
+//    res.render('admin/editar-vaga', { categorias, vaga })
+//})
+
+//app.post('/admin/vagas/editar/:id', async(req, res) => {
+//    const { titulo, descricao, categoria } = req.body
+//    const { id } = req.params
+//    const db = await dbConnection
+//    await db.run(`update vagas set categoria = ${categoria}, titulo = '${titulo}', descricao = '${descricao}' where id = ${id}`)
+//    res.redirect('/admin/vagas')
+//})
+
 const init = async() => {
     const db = await dbConnection
     await db.run('create table if not exists categorias (id INTEGER PRIMARY KEY, categoria TEXT);')
