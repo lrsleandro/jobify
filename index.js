fc20d8f390300f0b576d26728fb863cae214d3a9
lrsleandro <lrsleandro@gmail.com>
2020-02-26 13:54:57 -0300

    Correção Editar

--- index.js
+++ index.js
@@ -62,13 +62,13 @@ app.post('/admin/vagas/nova', async(req, res) => {
 app.get('/admin/vagas/editar/:id', async(req, res) => {
     const db = await dbConnection
     const categorias = await db.all('select * from categorias')
-    const vaga = await db.get('select * from vagas where id = ' + req.param.id)
+    const vaga = await db.get('select * from vagas where id = ' + req.params.id)
     res.render('admin/editar-vaga', { categorias, vaga })
 })
 
 app.post('/admin/vagas/editar/:id', async(req, res) => {
     const { titulo, descricao, categoria } = req.body
-    const { id } = req.param
+    const { id } = req.params
     const db = await dbConnection
     await db.run(`update vagas set categoria = ${categoria}, titulo = '${titulo}', descricao = '${descricao}' where id = ${id}`)
     res.redirect('/admin/vagas')
