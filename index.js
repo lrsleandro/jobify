c9a56cf0fa1ba961ecf1b5b112eb3c6d70df1e9d
lrsleandro <lrsleandro@gmail.com>
2020-02-27 17:36:46 -0300

    Teste

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const dbConnection = sqlite.open('banco.sqlite', { Promise })
 
 const port = process.env.PORT || 4200
 
-app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.set('views', path.join(__dirname, 'views'))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(bodyParser.urlencoded({ extended: true }))
 
